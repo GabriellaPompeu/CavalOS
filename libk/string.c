67c97a0f75621b119bb2bbc94682888c353beaf7
Daniel-Pita <danielsantospita@id.uff.br>
2026-09-05 10:51:44 -0300

    Update string.c

--- libk/string.c
+++ libk/string.c
@@ -57,6 +57,16 @@ int strcmp(const char* str1, const char* str2){
 	return 1;
 }
 
+int strncmp(const char* a, const char* b, size_t num){
+	int res = 0;
+
+	while(num > 0){
+		if((res = *a++ - *b++) != 0) return res;
+		--num;
+	}
+	return 0;
+}
+
 void* memmove(void* dest, const void* src, size_t num){
 	char* d = (char*) dest;
 	const char* origem = (char*) src;
@@ -87,4 +97,76 @@ int memcmp(const void* str1, const void* str2, size_t num){
 		--num;
 	}
 	return 0;
+}
+
+int strncasecmp(const char* str1, const char* str2, size_t num){
+	//Yes, Maria Clara, it had better be nice with your friends
+	char a, b;
+	int res = 0;
+
+	while(num > 0){
+		a = *str1++;
+		b = *str2++;
+		if(a == '\0' && b == '\0') return 0;
+		if(a >= 'A' && a <= 'Z') a += 'a' - 'A';
+		if(b >= 'A' && b <= 'Z') b += 'a' - 'A';
+		if((res = a - b) != 0) return res;
+		--num;
+	}
+	return 0;
+}
+
+int strcasecmp(const char* str1, const char* str2){
+	char a = *str1++, b = *str2++;
+	int res = 0;
+
+	while(a != '\0' || b != '\0'){
+		if(a >= 'A' && a <= 'Z') a += 'a' - 'A';
+		if(b >= 'A' && b <= 'Z') b += 'a' - 'A';
+		if((res = a - b) != 0) return res;
+		a = *str1++;
+		b = *str2++;
+	}
+	return a - b;
+}
+
+char* strchr(const char* str, int c){
+	while(*str != (char)c){
+		if(*str == '\0') return NULL;
+		str++;
+	}
+	return (char*)str;
+}
+
+char* strrchr(const char* str, int c){
+	char* tmp = NULL, valor = (char) c;
+
+	while(*str != '\0'){
+		if(*str == valor) tmp = str;
+		str++;
+	}
+	if(valor == '\0') return str;
+	return tmp;
+}
+
+char* strstr(const char* str1, const char* str2){
+	if(*str2 == '\0') return str1;
+
+	char* a = str1, *b = str2;
+
+	while(*str1 != '\0'){
+		if(*str1 != *b) str1++;
+
+		if(*str2 == *str1){
+			a = str1;
+			while(*str2 == *str1){
+				str1++; str2++;
+				if(*str2 == '\0') return a;
+				if(*str1 == '\0') return NULL;
+			}
+
+			str1 = ++a; str2 = b;
+		}
+	}
+	return NULL;
 }
