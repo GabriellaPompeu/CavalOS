c979568dca63080fb81904acf1f076b733feca5d
Daniel-Pita <danielsantospita@id.uff.br>
2026-09-12 13:02:43 -0300

    Update interrupts.h

--- interrupts.h
+++ interrupts.h
@@ -1,4 +1,9 @@
+#ifndef INTERRUPTS_H
+#define INTERRUPTS_H
+
 #include <stdint.h>
+
+#define IDT_ENTRIES 256
 
 struct idt_entry{
     uint16_t offset_low; //parte baixa do end da função q trata a inter.
@@ -6,5 +11,15 @@ struct idt_entry{
     uint8_t zero; //é literalmente um 0
     uint8_t type_attributes; //informa as características do Gate.
     uint16_t offset_high; //parte superior do end da função q trata a inter. 
-};
+}__attribute__((packed));
 
+struct idt_ptr{
+    uint16_t limit;
+    uint32_t base;
+}__attribute__((packed));
+
+void idt_init(void);
+
+void idt_set_gate(uint8_t index, uint32_t handler, uint16_t selector, uint8_t type_attributes);
+
+#endif
