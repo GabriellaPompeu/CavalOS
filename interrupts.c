0eb0f138cf42e818286d0f0e79be1d5aa8c80951
Daniel-Pita <danielsantospita@id.uff.br>
2026-09-13 22:37:24 -0300

    Update interrupts.c

--- interrupts.c
+++ interrupts.c
@@ -3,12 +3,10 @@
 #include <stdint.h> 
 #include "interrupts.h"
 
-//#define IDT_ENTRIES 256
-
 struct idt_entry idt[IDT_ENTRIES];
 struct idt_ptr idtp;
 
-void idt_set_gate(uint8_t index, uint32_t handler, uint16_t selector, uint8_t type_attributes){
+void idt_set_gate(uint8_t index, uint32_t handler, uint16_t selector, uint8_t type_attributes){ // dá os valores para a idt
     idt[index].offset_low = handler & 0xFFFF;
     idt[index].selector = selector;
     idt[index].zero = 0;
@@ -16,7 +14,7 @@ void idt_set_gate(uint8_t index, uint32_t handler, uint16_t selector, uint8_t ty
     idt[index].offset_high = (handler >> 16) & 0xFFFF;
 }
 
-void idt_init(void){
+void idt_init(void){ //inicializa a idt zerando todos os atributos e define o endereço que inicia e o maior byte
     for (size_t i = 0; i < IDT_ENTRIES; i++){
         idt[i].offset_low = 0;
         idt[i].selector = 0;
@@ -27,4 +25,29 @@ void idt_init(void){
 
     idtp.limit = sizeof(idt) - 1;
     idtp.base = (uint32_t)&idt;
+}
+
+static const char* exception_messages[32] = {
+    "Divisao por zero", "Debug",
+    "Interrupcao nao mascaravel", "Breakpoint",
+    "Overflow", "Limite excedido",
+    "Opcode invalido", "Dispositivo indisponivel",
+    "Double fault", "Coprocessor Segment Overrun",
+    "TSS invalido", "Segmento nao presente",
+    "Falha de segmento de pilha", "Falha de protecao geral",
+    "Falha de pagina", "Vaga",
+    "Ponto flutuante x87", "Alignment Check",
+    "Machine Check", "SIMD ponto flutuante",
+    "Virtualizacao", "Protecao controle",
+    "Vaga", "Vaga",
+    "Vaga", "Vaga",
+    "Vaga", "Vaga",
+    "Hypervisor Injection", "Comunicacao VMM",
+    "Seguranca", "Vaga"
+};
+
+void exception_handler(struct registers *r){
+    if (r->int_no < 32) {
+        // ainda vou criar a biblioteca stdio, então vamos poder por exemplo usar o printf para printas as mensagens
+    }
 }
