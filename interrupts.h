2faa3d68cc574537df43181364b0160234fc2b0b
Daniel-Pita <danielsantospita@id.uff.br>
2026-09-13 22:40:15 -0300

    Update interrupts.h

--- interrupts.h
+++ interrupts.h
@@ -13,10 +13,27 @@ struct idt_entry{
     uint16_t offset_high; //parte superior do end da função q trata a inter. 
 }__attribute__((packed));
 
-struct idt_ptr{
+struct idt_ptr{ //define o endereço que começa a idt e o maior byte que pode acessar
     uint16_t limit;
     uint32_t base;
 }__attribute__((packed));
+
+struct registers{
+    uint32_t ds;
+
+    uint32_t edi; uint32_t esi;
+    uint32_t ebp; uint32_t esp;
+    uint32_t ebx; uint32_t edx;
+    uint32_t ecx; uint32_t eax;
+
+    uint32_t int_no; uint32_t err_code;
+
+    uint32_t eip; uint32_t cs;
+    uint32_t eflags; uint32_t useresp;
+    uint32_t ss;
+};
+
+void exception_handler(struct registers *r);
 
 extern struct idt_entry idt[IDT_ENTRIES];
 extern struct idt_ptr idtp;
