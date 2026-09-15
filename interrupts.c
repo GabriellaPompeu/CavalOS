5f1fefd6b2f580851ad359ce72eeab550bd69d47
Daniel-Pita <danielsantospita@id.uff.br>
2026-09-14 22:17:50 -0300

    Update interrupts.c

--- interrupts.c
+++ interrupts.c
@@ -3,6 +3,8 @@
 #include <stdint.h> 
 #include "interrupts.h"
 
+extern void terminal_printf(const char* format, ...);
+
 struct idt_entry idt[IDT_ENTRIES];
 struct idt_ptr idtp;
 
@@ -46,8 +48,21 @@ static const char* exception_messages[32] = {
     "Seguranca", "Vaga"
 };
 
+static void kernel_panic(void){
+    terminal_printf("\nKERNEL DEU PANE!\n");
+
+    asm volatile("cli");
+    while(1) asm volatile("hlt");
+}
+
+static void handle_simple_exception(struct registers* r){
+    terminal_printf("\nEXCECAO #%d: %s\n", (int)r->int_no, exception_messages[r -> int_no]);
+    kernel_panic();
+}
+
 void exception_handler(struct registers *r){
     if (r->int_no < 32) {
+        handle_simple_exception(r);
         // ainda vou criar a biblioteca stdio, então vamos poder por exemplo usar o printf para printas as mensagens
     }
 }
